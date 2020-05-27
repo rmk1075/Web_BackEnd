c4c5ff6cf8e3e3a35c6c3b8928f8c0ac684fd1dd
jamie <rmk1075@gmail.com>
2020-05-27 20:50:31 +0900

    securityLogin/out

---
+++ securityexam/src/main/java/com/study/spring/securityexam/dao/MemberDaoSqls.java
@@ -0,0 +1,5 @@
+package com.study.spring.securityexam.dao;
+
+public class MemberDaoSqls {
+	public static final String SELECT_ALL_BY_EMAIL = "SELECT id, name, password, email, create_date, modify_date FROM member WHERE email = :email";
+}
